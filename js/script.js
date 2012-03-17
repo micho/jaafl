6c04867e69e8d704efd4c677e07546682ac1f6ba
Pablo Villalba <pablo@teambox.com>
2012-03-17 21:15:11 +0100

    Better REPLT, minor copy edits

--- js/script.js
+++ js/script.js
@@ -9,22 +9,52 @@
 $(function () {
 
   console.log = function (data) {
-    $(".log").append("<p class='data'>" + JSON.stringify(data) + "</p>");
+    var formatted;
+    try {
+      formatted = JSON.stringify(data);
+    } catch (e) {
+      formatted = data;
+    }
+    $(".log")
+      .append("<p class='data'>" + formatted + "</p>")
+      .scrollTop($(".log").prop("scrollHeight"));
   };
 
   console.info = function (message) {
-    $(".log").append("<p class='command'>" + message + "</p>");
+    $(".log")
+      .append("<p class='command'>" + message + "</p>")
+      .scrollTop($(".log").prop("scrollHeight"));
   };
 
   console.error = function (message) {
-    $(".log").append("<p class='error'>" + message + "</p>");
+    $(".log")
+      .append("<p class='error'>" + message + "</p>")
+      .scrollTop($(".log").prop("scrollHeight"));
   };
 
-  $(document).on("keyup", "textarea#console", function (e) {
-    var $textarea = $(e.target), $log = $(".log"), data, r;
+  var history = [];
+  history.pos = 0;
+
+  $(document).on("keydown", "textarea#console", function (e) {
     if (e.keyCode === 13) {
+      e.preventDefault();
+    }
+  });
+
+  $(document).on("keyup", "textarea#console", function (e) {
+    var $textarea = $(e.target), $log = $(".log"), data, r,
+        val = $textarea.val();
+    // Press enter
+    if (e.keyCode === 13) {
+      e.preventDefault();
+
+      // Move history
+      history.unshift(val);
+      history.pos = 0;
+
+      // Output results
       try {
-        data = eval("(" + $textarea.val() + ")");
+        data = eval("(" + (val.length ? val : "undefined") + ")");
         console.info($textarea.val());
         console.log(data);
       } catch (exc) {
@@ -32,9 +62,21 @@ $(function () {
         console.error(exc);
       }
       $textarea.val("");
-      $log.scrollTop($log.prop("scrollHeight"));
+    }
+
+    // Navigate history
+    if (e.keyCode === 38) {
+      history.pos = Math.max(Math.min(history.pos + 1, history.length), 0);
+      $textarea.val(history[history.pos - 1]);
+    } else if (e.keyCode === 40) {
+      history.pos = Math.max(history.pos - 1, 0);
+      $textarea.val(history[history.pos - 1]);
+    } else {
+      history.pos = 0;
     }
   });
+
+  console.info("<br>This is your interactive JS console.<br><br><br>Use it to try new commands as you read this guide.<br><br>It works pretty much like a calculator.<br>Try typing these and see what you get:<br><br>&nbsp;&nbsp;&nbsp;&nbsp;a = 27 + 15<br>&nbsp;&nbsp;&nbsp;&nbsp;Math.min(2,5)<br>&nbsp;&nbsp;&nbsp;&nbsp;'Hello, world!'<br>&nbsp;&nbsp;&nbsp;&nbsp;alert('hey!')<br><br>Let the coding begin!<br><br>");
 
 });
       
